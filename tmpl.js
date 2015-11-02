9d24332215a1b4bdca7abfde5bd4add150c43317
Thomas Hunkapiller <tom@furycodes.com>
2015-11-02 17:16:11 -0600

    Update tmpl.js

--- tmpl.js
+++ tmpl.js
@@ -1,5 +1,4 @@
 String.prototype.tmpl = function(d) {
-        return this.replace(
-                /\$\$\s*([^$]+)\s*\$\$/g,
-                (new Function('d', 'return function(_,s){with(d) return eval(s)}'))(d||{}))
+	return this.replace(/\$\$([^$]+)\$\$/g,
+		Function('d', 'return function(_,s){with(d) return eval(s)}')(d||{}))
 };
